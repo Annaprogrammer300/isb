60f79e238e2f8d0aad6755fdbd0d7bef716676ff
Annaprogrammer300 <star2004.as@gmail.com>
2024-03-17 23:40:48 +0400

    encoder and decoder with bugs

--- lab_1/task1.py
+++ lab_1/task1.py
@@ -2,11 +2,18 @@ import os
 
 from works_files import *
 
-alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя .,?/!()- \n "
+alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя :.,?/!()- \n "
 
 
 def encrypt(path_key: str, text_path: str, path: str) -> None:
+    """
+    implements a cipher with a key and writes the data to a file
 
+    Parameters
+        path_key: the path to the key file
+        text_path: path to the file where the message is located
+        path: the path where the cipher will be written
+    """
     key = read_json(path_key)
     result = ''
     text = read_files(text_path)
@@ -15,40 +22,52 @@ def encrypt(path_key: str, text_path: str, path: str) -> None:
             result += key[char]
         else:
             result += char
-        write_files(path, result)
-    else:
-        print("Ошибка: Не удалось прочитать текст из файла.")
+    write_files(path, result)
 
 
 def decrypt(path_key: str, path_encryption: str, path_decryption: str) -> None:
+    """
+    Decrypts the text by the key
 
+    Parameters
+        path_key: the path to the key file
+        path_encryption: the path to the encrypted text file
+        path_decryption: the path to the file where the decrypted text will be written
+    """
     key = read_json(path_key)
     result = ''
     text = read_files(path_encryption)
     for char in text:
-        if char in key:
-            result += key[char]
-        else:
-            result += char
-        write_files(path_decryption, result)
-    else:
-        print("Ошибка: Не удалось прочитать текст из файла.")
+        found_key = None
+        for k, v in key.items():
+            if v == char:
+                found_key = k
+                break
+
+        result += found_key
+    write_files(path_decryption, result)
 
 
 def key_json(key: str, path: str) -> None:
-    sz = len(key)  # Получаем размер ключа
+    """
+    Create a key to the text using the specified key value and write it to a json file as a dictionary
+
+    Parameters
+        key: the values of the key that will be used to create a new one
+        path: the path where the key will be written
+    """
+    sz = len(key)
     key = dict()
     num_rows = -(-len(alphabet) // sz)
     matrix = [['' for _ in range(sz)] for _ in range(num_rows)]
 
     index = 0
     for row in range(num_rows):
-        for col in range(sz):  # Исправлено
+        for col in range(sz):
             if index < len(alphabet):
                 matrix[row][col] = alphabet[index]
                 index += 1
 
-    # Шифруем текст по столбцам согласно ключу
     ciphertext = ''
     for col in range(sz):
         for row in range(num_rows):
@@ -62,10 +81,10 @@ def key_json(key: str, path: str) -> None:
 if __name__ == "__main__":
     keys = "43521"
 
-    # Создание ключа для шифра и запись в файл
     key_json(keys, os.path.join('first_task', 'key.json'))
 
     encrypt(os.path.join('first_task', 'key.json'), os.path.join('first_task', 'text_path.txt'),
             os.path.join('first_task', 'encryption.txt'))
+
     decrypt(os.path.join('first_task', 'key.json'), os.path.join('first_task', 'encryption.txt'),
             os.path.join('first_task', 'decryption.txt'))
