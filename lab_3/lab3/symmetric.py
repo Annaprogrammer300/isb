db16726b9a8f1055664292938cadc7ed44f31b95
Annaprogrammer300 <star2004.as@gmail.com>
2024-05-11 16:25:58 +0400

    added new methods for working with a symmetric key

--- lab_3/lab3/symmetric.py
+++ lab_3/lab3/symmetric.py
@@ -2,11 +2,28 @@ import os
 
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import padding
-from works_files import write_files
+from works_files import (write_files, SerializeMethod, serialize_symmetric_key,
+                         deserialize_symmetric_key, write_bytes, read_bytes)
 
 
-class Symmetric:
+class SymmetricEncryption:
     """
     A class that implements symmetric encryption using the IDEA algorithm.
 
+    Attributes:
+        key: encryption key
     """
+
+    def __init__(self):
+        self.key = None
+
+    def generate_key(self) -> bytes:
+        """
+        Generates a random 16 byte encryption key.
+
+        Returns
+            The generated encryption key.
+        """
+        self.key = os.urandom(16)
+        return self.key
+
--- lab_3/lab3/works_files.py
+++ lab_3/lab3/works_files.py
@@ -1,4 +1,10 @@
 import json
+from enum import Enum
+
+
+class SerializeMethod(Enum):
+    SERIALIZE_SYMMETRIC_KEY = 1
+    DESERIALIZE_SYMMETRIC_KEY = 2
 
 
 def write_files(path: str, data: str) -> None:
@@ -34,3 +40,74 @@ def read_json(path: str) -> dict:
         print(f"The file '{path}' was not found")
     except Exception as e:
         print(f"An error occurred while reading the JSON file: {str(e)}")
+
+
+def read_bytes(file_path: str) -> bytes:
+    """
+    Reads the contents of a file in binary format.
+
+    Parameters
+        file_path: The path to the file to be read.
+    Returns
+        The contents of the file in binary format.
+    """
+    try:
+        with open(file_path, "rb") as file:
+            data = file.read()
+        return data
+    except FileNotFoundError:
+        print("The file was not found")
+    except Exception as e:
+        print(f"An error occurred while reading the file: {str(e)}")
+
+
+def write_bytes(file_path: str, bytes_text: bytes) -> None:
+    """
+    Writes binary data to a file.
+
+    Parameters
+        file_path: The path to the file where the data will be written.
+        bytes_text: The binary data to be written to the file.
+    """
+    try:
+        with open(file_path, "wb") as file:
+            file.write(bytes_text)
+        print(f"The data has been successfully written to the file '{file_path}'.")
+    except FileNotFoundError:
+        print("The file was not found")
+    except Exception as e:
+        print(f"An error occurred while writing the file: {str(e)}")
+
+
+def serialize_symmetric_key(self, path: str) -> None:
+    """
+           Serializes the encryption key to a file.
+
+           Parameters
+               path: The path to the file where the encryption key will be saved.
+           """
+    try:
+        with open(path, 'wb') as key_file:
+            key_file.write(self.key)
+        print(f"The symmetric key has been successfully written to the file '{path}'.")
+    except FileNotFoundError:
+        print("The file was not found")
+    except Exception as e:
+        print(f"An error occurred while writing the file: {str(e)}")
+
+
+def deserialize_symmetric_key(self, file_name: str) -> None:
+    """
+           Deserializes the encryption key from a file.
+           Parameters
+               file_name: The path to the file containing the encryption key.
+           """
+    with open(file_name, "rb") as file:
+        self.key = file.read()
+    try:
+        with open(file_name, "rb") as file:
+            self.key = file.read()
+    except FileNotFoundError:
+        print("The file was not found")
+    except Exception as e:
+        print(f"An error occurred while reading the file: {str(e)}")
