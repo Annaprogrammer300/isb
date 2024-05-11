c52a75b625440fad2085a00f4f55e873dc635789
Annaprogrammer300 <star2004.as@gmail.com>
2024-05-12 01:44:05 +0400

    start asymmetric

---
+++ lab_3/lab3/asymmetric.py
@@ -0,0 +1,14 @@
+
+class AsymmetricEncryption:
+    """
+    A class that implements asymmetric encryption and decryption using the RSA algorithm.
+
+    Attributes
+        private_key: The private key.
+        public_key: The public key.
+    """
+
+    def __init__(self):
+        self.private_key = None
+        self.public_key = None
+
